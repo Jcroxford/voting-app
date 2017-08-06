a359f09eb1607c31664111386048d841d9f0e048
Jesse Croxford <takeflightsc@gmail.com>
2017-08-06 13:04:58 -0600

    added db connection boilerplate for local environment

--- server/server.js
+++ server/server.js
@@ -8,10 +8,25 @@ const path       = require('path')
 const index = require('./routes/index')
 
 require('./config/config')
+
 // environment variables
 const app  = express()
 const port = process.env.PORT
 
+// database connect
+const Sequelize = require('sequelize')
+
+const {database, dbUsername, dbPassword, dialect} = process.env
+
+const sequelize = new Sequelize(database, dbUsername, dbPassword, {
+  dialect
+})
+
+sequelize.authenticate()
+  .then( () => console.log('connected successfully'))
+  .catch ( err => console.log('connection failed:', err))
+
+// middleware
 // TODO: app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
