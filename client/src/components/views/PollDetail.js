6fca0aec03b8388fa16fa57bd3801ded8bddfed4
Jesse Croxford <takeflightsc@gmail.com>
2017-08-27 23:43:21 -0600

    added initial chart.js display to pollDetail component

--- client/src/components/views/PollDetail.js
+++ client/src/components/views/PollDetail.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import axios from 'axios'
+import {HorizontalBar} from 'react-chartjs-2'
 
 import {baseRoute} from '../../utils/api'
 
@@ -8,47 +9,117 @@ class PollDetail extends Component {
     super()
 
     this.state = {
-      pollOptions: []
+      pollOptions: [],
+      selectedOptionId: 0,
+      userHasVoted: false
     }
 
-    this.renderPollOptions = this.renderPollOptions.bind(this)
+    this.handleSelectedOptionChange = this.handleSelectedOptionChange.bind(this)
     this.updateVotes = this.updateVotes.bind(this)
   }
 
+  // *** render html ***
+  renderVoteForm() {
+    return (
+      <form className="uk-form-stacked">
+
+        <div className="uk-margin">
+          <div className="uk-form-controls">
+            {this.renderPollOptions()}
+          </div>
+        </div>
+
+        <div className="uk-margin">
+          <button className="uk-button uk-button-primary" onClick={this.updateVotes}>Vote</button>
+        </div>
+
+      </form>
+    )
+  }
   renderPollOptions() {
     return this.state.pollOptions.map(option => (
-      <li key={option.id}>
-        <strong 
-          onClick={() => this.updateVotes(option.id)} 
-          style={ { cursor: 'pointer' } }
-        >
-          Vote
-        </strong>
-        {` ${option.pollText}`} vote count {option.voteCount}
-      </li>
+      <div className="uk-margin" key={option.id}>
+        <label onClick={() => this.handleSelectedOptionChange(option.id)}>
+          <input
+            className="uk-radio" 
+            type="radio"
+            name="pollOption"
+          />
+          {` ${option.pollText}`}
+        </label>
+      </div>
+      
     ))
   }
 
-  updateVotes(optionId) {
+  renderPollResults() {
+    const {pollOptions} = this.state
+    const labels = pollOptions.map(option => option.pollText)
+    const voteData = pollOptions.map(option => option.voteCount)
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: false,
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: voteData
+        }
+      ]
+    }
+    const options = {
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            drawOnChartArea: false
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+            drawOnChartArea: false
+          }
+        }]
+      }
+    }
+
+    return (
+      <div>
+        <HorizontalBar data={data} options={options} />
+      </div>
+    )
+  }
+
+  // *** event handlers & react functions ***
+  handleSelectedOptionChange(optionId) {
+    this.setState({ selectedOptionId: optionId })
+  }
+
+  updateVotes(e) {
+    e.preventDefault()
+
     const poll = this
     const pollId = poll.props.match.params.pollId
+    const {selectedOptionId, pollOptions} = this.state
 
     // stores and keeps track of a particular user's votes via localStorage
-    let localVotes = JSON.parse(localStorage.getItem('localVotes'))
-
     // localVotes is an array of poll Ids
-    if(!localVotes) { localVotes = [] }
+    let localVotes = JSON.parse(localStorage.getItem('localVotes')) || []
 
     if(localVotes.includes(pollId)) { return alert('you have already voted on this poll!') }
 
-    axios.get(`${baseRoute}/api/poll/vote/${optionId}`)
+    axios.get(`${baseRoute}/api/poll/vote/${selectedOptionId}`)
       .then(response => {
-        const pollOptions = poll.state.pollOptions
-        const pollOptionIndex = pollOptions.findIndex(option => option.id === optionId)
+        const pollOptionIndex = pollOptions.findIndex(option => option.id === selectedOptionId)
 
         pollOptions[pollOptionIndex].voteCount = response.data.updatedVoteCount
 
-        poll.setState({ pollOptions })
+        poll.setState({ pollOptions, userHasVoted: true })
 
         // update local storage to prevent user from voting on a poll multiple times
         localVotes.push(pollId)
@@ -59,19 +130,35 @@ class PollDetail extends Component {
 
   componentWillMount() {
     const poll = this
-    axios.get(`${baseRoute}/api/polls/detail/${this.props.match.params.pollId}`)
+    const {pollId} = this.props.match.params
+
+     // populate pollOptions
+    axios.get(`${baseRoute}/api/polls/detail/${pollId}`)
       .then(response => poll.setState({ pollOptions: response.data.pollOptions }))
       .catch(error => console.log(error))
+
+    // check localVotes for confirming wether or not use has voted
+    const localVotes = JSON.parse(localStorage.getItem('localVotes')) || []
+
+    const userHasVoted = ~localVotes.findIndex(votedPollId => votedPollId === pollId)
+    
+    this.setState({ userHasVoted })
   }
 
   render() {
     return (
       this.state.pollOptions.length === 0
-      ? <div>poll detail page</div>
-      : <div>
-          <ul>
-            {this.renderPollOptions()}
-          </ul>
+      ? <div>Loading</div>
+      : <div className="uk-flex uk-flex-center">
+          <div className="uk-card uk-card-default uk-width-1-2@s uk-width-1-3@m uk-animation-slide-top">
+            <div className="uk-card-header uk-card-primary">
+              <h3 className="uk-card-title">{this.props.match.params.pollTitle}</h3>
+            </div>
+            
+            <div className="uk-card-body">
+              {this.state.userHasVoted ? this.renderPollResults() : this.renderVoteForm()}
+            </div>
+          </div>
         </div>
     )
   }
