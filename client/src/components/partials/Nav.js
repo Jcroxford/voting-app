252a2af6ff09531aca01f8cc256111e5c80a3303
Jesse Croxford <takeflightsc@gmail.com>
2017-08-29 13:09:43 -0600

    added custom styling to nav
    changed nav color to blue with white text.
    added persistance to show selected nav option(brighter white text)
    added custom box shadow to nav.

--- client/src/components/partials/Nav.js
+++ client/src/components/partials/Nav.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Link, withRouter} from 'react-router-dom'
 
+import '../../styles/Nav.css'
+
 class Nav extends Component {
   constructor(props) {
     super(props)
@@ -18,17 +20,21 @@ class Nav extends Component {
 
   render() {
     const authenticated = this.props.authenticated
+    const currentPath = this.props.location.pathname
 
     return (
       <div>
-        <nav className="uk-navbar uk-navbar-container uk-box-shadow-medium">
+        <nav className="uk-navbar uk-navbar-container">
+          <div className="uk-navbar-left">
+            
+          </div>
           <div className="uk-navbar-right">
             <ul className="uk-navbar-nav">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/polls">All Polls</Link></li>
-              {authenticated ? '' : <li><Link to="/signin">Signin</Link></li>}
-              {authenticated ? '' : <li className="uk-margin-right"><Link to="/signup">Signup</Link></li>}
-              {authenticated ? <li><Link to="/settings">Settings</Link></li> : ''}
+              <li className={currentPath === '/' ? 'uk-active' : ''}><Link to="/">Home</Link></li>
+              <li className={currentPath === '/polls' ? 'uk-active' : ''}><Link to="/polls">All Polls</Link></li>
+              {authenticated ? '' : <li className={currentPath === '/signin' ? 'uk-active' : ''}><Link to="/signin">Signin</Link></li>}
+              {authenticated ? '' : <li className={`uk-margin-right ${currentPath === '/signup' ? 'uk-active' : ''}`}><Link to="/signup">Signup</Link></li>}
+              {authenticated ? <li className={currentPath === '/settings' ? 'uk-active' : ''}><Link to="/settings">Settings</Link></li> : ''}
               {authenticated ? <li className="uk-margin-right"><a onClick={this.handleSignout}>Sign Out</a></li> : ''}
             </ul>
           </div>
