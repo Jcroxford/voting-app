668f7d64aba2709b2d5a345ca9485813821ad538
Jesse Croxford <takeflightsc@gmail.com>
2017-09-01 23:53:15 -0600

    restructured fade and load animations on Polls component
    fixed load animation. Animation now spins in the center of the screen and is only visible at the appropriate times.
    Also modified polls fade animation to cascade and happen during scrolling via uk scrollspy

--- client/src/components/views/Polls.js
+++ client/src/components/views/Polls.js
@@ -11,7 +11,7 @@ class Polls extends Component {
       polls: [],
       page: 1,
       totalPages: 1,
-      fetchingPolls: false
+      fetchingPolls: true
     }
 
     this.handleScroll = this.handleScroll.bind(this)
@@ -27,7 +27,12 @@ class Polls extends Component {
           polls.push(poll)
         }
         
-        self.setState({ polls, totalPages: Math.ceil(response.data.totalPolls / 36), fetchingPolls: false })
+        // self.refs.ukSpinner.removeAttribute('uk-spinner')
+        self.setState({
+          polls,
+          totalPages: Math.ceil(response.data.totalPolls / 36),
+          fetchingPolls: false
+        })
       })
       .catch(error => console.log(error))
   }
@@ -37,7 +42,7 @@ class Polls extends Component {
       return (
         <div key={poll.id}>
           <div 
-            className="uk-card uk-card-default uk-margin-bottom uk-card-hover uk-animation-fade" 
+            className="uk-card uk-card-default uk-margin-bottom uk-card-hover" 
             onClick={() => this.props.history.push(`/poll/${poll.id}/${poll.title}`)}
           >
             <div className="uk-card-body">
@@ -57,11 +62,16 @@ class Polls extends Component {
     const { page, totalPages } = this.state
 
     if(clientHeight + amountScrolled >= closeToPageBottom && page < totalPages && !this.state.fetchingPolls) {
-      this.setState({ page: page + 1, fetchingPolls: true })
+      this.setState({ page: page + 1, fetchingPolls: true }, () => this.refs.ukSpinner.setAttribute('uk-spinner', ''))
       this.getPolls()
     }
   }
 
+  componentDidMount() {
+    this.refs.ukSpinner.setAttribute('uk-spinner', '')
+    this.refs.ukScrollspy.setAttribute('uk-scrollspy', 'target: > div; cls:uk-animation-fade uk-transform-origin-bottom-right; delay: 25')
+  }
+  
   componentWillMount() {
     this.getPolls()
     window.addEventListener('scroll', this.handleScroll)
@@ -73,13 +83,16 @@ class Polls extends Component {
 
   render() {
     return (
-      this.state.polls.length === 0
-      ? <div className="uk-spinner"></div>
-      : <div>
-          <div className="uk-child-width-1-2@s uk-child-width-1-3@m uk-child-width-1-4@l uk-grid">
+       <div>
+          <div className="uk-child-width-1-2@s uk-child-width-1-3@m uk-child-width-1-4@l uk-grid" ref="ukScrollspy">
             {this.renderPolls()}
           </div>
-          {/* {this.state.fetchingPolls ? <div className="uk-spinner uk-center-text">uk spinner should be here</div> : ''} FIXME: uk-spinner can't be added */}
+          
+          {
+            this.state.fetchingPolls 
+              ? <div className="uk-flex uk-flex-center" ref="ukSpinner"></div>
+              : ''
+          }
         </div>
     )
   }
